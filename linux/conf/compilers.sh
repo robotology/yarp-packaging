80231ad75691fb091fdc069d406fcb2442367829
Matteo Brunettini <matteo.brunettini@iit.it>
2019-10-25 09:02:04 +0200

    new release 3.2.1

--- linux/conf/compilers.sh
+++ linux/conf/compilers.sh
@@ -1,11 +1,10 @@
-#PLATFORMS="bionic cosmic disco stretch"
-PLATFORMS="bionic cosmic disco buster"
+PLATFORMS="bionic disco eoan stretch buster"
 
-buster_MIRROR="http://httpredir.debian.org/debian"
-stretch_MIRROR="http://httpredir.debian.org/debian"
+buster_MIRROR="http://deb.debian.org/debian"
+stretch_MIRROR="http://deb.debian.org/debian"
 bionic_MIRROR="http://it.archive.ubuntu.com/ubuntu"
-cosmic_MIRROR="http://it.archive.ubuntu.com/ubuntu"
 disco_MIRROR="http://it.archive.ubuntu.com/ubuntu/"
+eoan_MIRROR="http://it.archive.ubuntu.com/ubuntu/"
 
 HARDWARE="i386 amd64"
 
